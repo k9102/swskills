7006d268495a05eba5edce1080a219febcbc7772
gary kim <gary.kimm@gmail.com>
2019-09-19 08:43:43 +0900

    code is organized using range for

--- bigdata.cpp
+++ bigdata.cpp
@@ -20,22 +20,22 @@ void Solve() {
 
 	//	코드를 작성하세요
 
-	for (int i = 0; i < N; i++) 
+	for (int i = 0; i < N; i++)
 	{
 		auto& m = idx[str[i]];
 		if (m == 0)
 		{
-			tbl.emplace_back(str[i], vector<int>{i+1});
+			tbl.emplace_back(str[i], vector<int>{i + 1});
 			m = tbl.size();
 		}
 		else
 		{
-			tbl[m-1].second.push_back(i + 1);
+			tbl[m - 1].second.push_back(i + 1);
 		}
-	
+
 	}
 
-	stable_sort(begin(tbl), end(tbl), [](const auto &a, const auto &b) 
+	stable_sort(begin(tbl), end(tbl), [](const auto &a, const auto &b)
 	{
 		return (a.second.size() > b.second.size());
 	});
@@ -47,23 +47,23 @@ void Solve() {
 	else
 	{
 		int acc = 0;
-		int i = 0;
-		while( acc < N / 2  && i < tbl.size())
+		for (const auto &[name, vec] : tbl)
 		{
-			const auto &[name,vec] = tbl[i++];
-
+			if (acc >= N / 2)
+				break;
 			if (vec.size() < 2)
 				break;
 
 			cout << name;
-			for (int j = 0; j < vec.size(); j++)
+			for (const auto pos : vec)
 			{
-				cout << " " <<vec[j];
+				cout << " " << pos;
 			}
 			cout << endl;
+
 			acc += vec.size();
 		}
-		
+
 	}
 }
 
