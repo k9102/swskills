6571b091e28cdc7cfa3592b7de1da20617aed8b4
김규열(k9102) <k9102@lginnotek.com>
2019-09-21 18:50:10 +0900

    first trial for takeawalk

---
+++ takeawalk.cpp
@@ -0,0 +1,71 @@
+#include "pch.h"
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <iterator>
+#include <limits.h>
+
+using namespace std;
+
+int N;//직원 수
+int T;//산책 시간(분단위)
+int P[100000 + 10];//직원 출발 위치
+int S[100000 + 10];//직원 산책 속도(분당)
+
+map<int, int> spd_;
+
+void InputData() {
+	cin >> N >> T;
+	for (int i = 0; i < N; i++) {
+		cin >> P[i] >> S[i];
+	}
+}
+
+int main() {
+	int ans = -1;
+
+	InputData();			//	입력 함수
+
+	//	코드를 작성하세요
+
+	for (int i = 0; i < N; i++)
+	{
+		if (spd_.find(P[i]) == end(spd_))
+		{
+			spd_[P[i]] = S[i];
+		}
+		else
+		{
+			spd_[P[i]] = min(S[i], spd_[P[i]]);
+		}
+	}
+	
+	int nearest_loc = INT_MAX;
+	int grp = 0;
+	for_each(rbegin(spd_), rend(spd_), [&nearest_loc,&grp](auto speed) {
+		auto[p, s] = speed;
+		auto loc = p + s * T;
+
+		if (loc < nearest_loc)
+		{
+			grp++;
+			nearest_loc = loc;
+		}
+
+	});
+
+	ans = grp;
+
+	cout << ans << endl;	//	정답 출력
+	return 0;
+}
+
+/*
+5 3
+0 1
+1 2
+2 3
+3 2
+6 1
+
+3*/
